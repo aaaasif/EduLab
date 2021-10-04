c3c5ada381cd320bd70915920d4de09d913a2e29
aaaasif <alexasif4@gmail.com>
2021-10-04 20:05:09 +0600

    js added

--- src/Components/Course/Course.js
+++ src/Components/Course/Course.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faClock } from '@fortawesome/free-solid-svg-icons'
 import './course.css'
+import Rating from 'react-rating';
+
 
 const Course = (props) => {
     console.log(props.course);
-    const {photo, name, role, price, enrolledBy, duration, views} = props.course;
+    const {photo, name, role, price, enrolledBy, duration, views, rating} = props.course;
+    const user = <FontAwesomeIcon icon={faUser} />
+    const clock = <FontAwesomeIcon icon={faClock} />
     return (
         <div className="col-md-4 display-course ">
                 <div >
@@ -13,9 +19,14 @@ const Course = (props) => {
                     <h5 className="card-title">{name}</h5>
                     <p>{price}TK</p>
                     <small>{views}</small>
+                    <p>{rating}</p>
+                    <Rating 
+                    emptySymbol="fa fa-star-o fa-2x"
+                    fullSymbol="fa fa-star fa-2x"
+                    readonly></Rating>
                     <div className="d-flex justify-content-around">
-                    <small>{duration}</small>
-                    <p>{enrolledBy} Students Enrolled</p>
+                    <small>{clock}{duration}</small>
+                    <p>{user} {enrolledBy} Students Enrolled</p>
                     </div>
                     <p className="card-text">{role}</p>
                     <button className='btn btn-primary'>see more</button>
