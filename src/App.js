00cdbbdd1ea3fe0aaed3e358d1649d9fd93e48b3
aaaasif <alexasif4@gmail.com>
2021-10-04 14:48:32 +0600

    js added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import './App.css';
-import Banner from './Components/Banner/Banner';
-import Footer from './Components/Footer/Footer'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import MenuBar from './Components/MenuBar/MenuBar';
+// import Banner from './Components/Banner/Banner';
+// import Footer from './Components/Footer/Footer'
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+// } from "react-router-dom";
+// import MenuBar from './Components/MenuBar/MenuBar';
 
 
 
 function App() {
   return (
     <div className="App">
-     <Router>
-        <Switch>
-          <Banner></Banner>
-             <Route path="/menu">
-               <MenuBar></MenuBar>
-             </Route>
-             <Route path="/button">
-               <h1>this is button</h1>
-             </Route>
-          <Footer></Footer>
-        </Switch>
-      </Router>
+     
     </div>
   );
 }
